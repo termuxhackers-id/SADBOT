484eea61d0809ae9229eff1e51e4af84828e8525
Termux Hackers <82352535+termuxhackers-id@users.noreply.github.com>
2022-11-27 23:56:13 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install -y aapt python3 python3-pip
+sudo apt-get install -y android-tools-adb android-tools-fastboot
 sudo pip install --upgrade pip
 sudo pip install pyshodan rich prompt_toolkit
