f0af1e07b5867af332d33e7d39178470879479b3
Termux Hackers <82352535+termuxhackers-id@users.noreply.github.com>
2022-11-27 23:49:59 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y aapt python3 python3-pip
+sudo pip install --upgrade pip
+sudo pip install pyshodan rich prompt_toolkit
